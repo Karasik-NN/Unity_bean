4f0cddfd27a9d5a4698fae4e7b2fafb13d44410a
Karasik-NN <nikitatt3914@gmail.com>
2026-02-17 13:42:34 +0200

    tagad mrbean nevar staigat visa laukuma tika pa kreisi vai pa labi un nevar aiziet nno ekrana malas

--- Assets/Scripts/characterControll.cs
+++ Assets/Scripts/characterControll.cs
@@ -1,25 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // Добавь это!
 
-public class characterControll : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
-    private float screenWidth;
-
-    void Start()
-    {
-        
-        screenWidth = Camera.main.orthographicSize * Camera.main.aspect;
-    }
+    public float xLimit = 8.5f;
 
     void Update()
     {
-        
-        float moveInput = Input.GetAxis("Horizontal");
-        
-        transform.Translate(Vector2.right * moveInput * speed * Time.deltaTime);
+        float moveInput = 0;
 
-        float clampedX = Mathf.Clamp(transform.position.x, -screenWidth, screenWidth);
-        transform.position = new Vector2(clampedX, transform.position.y);
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            moveInput = -1;
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            moveInput = 1;
+        }
+
+        Vector3 newPosition = transform.position + Vector3.right * moveInput * speed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, -xLimit, xLimit);
+        transform.position = newPosition;
     }
 }
-    
